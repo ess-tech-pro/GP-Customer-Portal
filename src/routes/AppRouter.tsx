1f6f37a99ed5254658a69c85025967a7f431fd38
Wally <wally@ess-tech.pro>
2025-02-04 10:57:57 +0700

    chore: remove file redundancy

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -12,55 +12,55 @@ const GameList = lazy(() => import('../pages/game-list'))
 const Examples = lazy(() => import('../pages/examples'))
 
 const routesConfig = [
-	{
-		path: ROUTE_PATH.HOME,
-		layout: MainLayout,
-		component: Home,
-	},
-	{
-		path: ROUTE_PATH.REGISTER,
-		layout: AuthLayout,
-		component: () => <div>Register</div>,
-	},
-	{
-		path: ROUTE_PATH.LOGIN,
-		layout: AuthLayout,
-		component: Login,
-	},
-	{
-		path: ROUTE_PATH.GAME.DETAIL,
-		layout: MainLayout,
-		component: GameDetail,
-	},
-	{
-		path: ROUTE_PATH.GAME.LIST,
-		layout: MainLayout,
-		component: GameList,
-	},
-	{
-		path: ROUTE_PATH.EXAMPLES,
-		layout: MainLayout,
-		component: Examples,
-	},
+  {
+    path: ROUTE_PATH.HOME,
+    layout: MainLayout,
+    component: Home,
+  },
+  {
+    path: ROUTE_PATH.REGISTER,
+    layout: AuthLayout,
+    component: () => <div>Register</div>,
+  },
+  {
+    path: ROUTE_PATH.LOGIN,
+    layout: AuthLayout,
+    component: Login,
+  },
+  {
+    path: ROUTE_PATH.GAME.DETAIL,
+    layout: MainLayout,
+    component: GameDetail,
+  },
+  {
+    path: ROUTE_PATH.GAME.LIST,
+    layout: MainLayout,
+    component: GameList,
+  },
+  {
+    path: ROUTE_PATH.EXAMPLES,
+    layout: MainLayout,
+    component: Examples,
+  },
 ]
 
 const createAppRouter = () =>
-	createBrowserRouter(
-		routesConfig.map(({ path, layout: Layout, component: Component }) => ({
-			path,
-			errorElement: <RouterError />,
-			element: (
-				<Layout>
-					<Suspense fallback={<div>Loading...</div>}>
-						<Component />
-					</Suspense>
-				</Layout>
-			),
-		}))
-	)
+  createBrowserRouter(
+    routesConfig.map(({ path, layout: Layout, component: Component }) => ({
+      path,
+      errorElement: <RouterError />,
+      element: (
+        <Layout>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Component />
+          </Suspense>
+        </Layout>
+      ),
+    }))
+  )
 
 export function AppRouter() {
-	const router = useMemo(() => createAppRouter(), [])
+  const router = useMemo(() => createAppRouter(), [])
 
-	return <RouterProvider router={router} />
+  return <RouterProvider router={router} />
 }
