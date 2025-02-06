783d3f8154d27ab34865acc2aa2df73db5bea7db
kenEssTechPro <ken@ess-tech.pro>
2025-02-06 14:53:11 +0700

    fix: remove navigation game detail

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -2,59 +2,53 @@ import { ROUTE_PATH } from '@/constants/routing'
 import DashboardIcon from '@mui/icons-material/Dashboard'
 import DnsIcon from '@mui/icons-material/Dns'
 import InfoIcon from '@mui/icons-material/Info'
-import PeopleIcon from '@mui/icons-material/People'
 import SettingsIcon from '@mui/icons-material/Settings'
 
 const navigation = [
-	{
-		title: 'Main',
-		links: [
-			{
-				path: '/',
-				title: 'Daily Win',
-				icon: <DashboardIcon />,
-			},
-			{
-				title: 'Games',
-				path: ROUTE_PATH.GAME,
-				icon: <SettingsIcon />,
-				children: [
-					{
-						path: ROUTE_PATH.GAME,
-						title: 'Game list',
-						icon: <DnsIcon />,
-					},
-					{
-						path: ROUTE_PATH.GAME_DETAIL,
-						title: 'Game detail',
-						icon: <PeopleIcon />,
-					},
-				],
-			},
-			{
-				title: 'Game Management',
-				path: ROUTE_PATH.GAME_MANAGEMENT.CREATE_GAME,
-				icon: <SettingsIcon />,
-				children: [
-					{
-						path: ROUTE_PATH.GAME_MANAGEMENT.CREATE_GAME,
-						title: 'Create Game',
-						icon: <DnsIcon />,
-					},
-				],
-			},
-		],
-	},
-	{
-		title: 'Info',
-		links: [
-			{
-				path: '/about',
-				title: 'About',
-				icon: <InfoIcon />,
-			},
-		],
-	},
+  {
+    title: 'Main',
+    links: [
+      {
+        path: '/',
+        title: 'Daily Win',
+        icon: <DashboardIcon />,
+      },
+      {
+        title: 'Games',
+        path: ROUTE_PATH.GAME,
+        icon: <SettingsIcon />,
+        children: [
+          {
+            path: ROUTE_PATH.GAME,
+            title: 'Game list',
+            icon: <DnsIcon />,
+          },
+        ],
+      },
+      {
+        title: 'Game Management',
+        path: ROUTE_PATH.GAME_MANAGEMENT.CREATE_GAME,
+        icon: <SettingsIcon />,
+        children: [
+          {
+            path: ROUTE_PATH.GAME_MANAGEMENT.CREATE_GAME,
+            title: 'Create Game',
+            icon: <DnsIcon />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Info',
+    links: [
+      {
+        path: '/about',
+        title: 'About',
+        icon: <InfoIcon />,
+      },
+    ],
+  },
 ]
 
 export default navigation
