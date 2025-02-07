500543a48c2d754c5987cb9ff5097ccc7bc7410c
John <John.com>
2025-02-07 13:54:26 +0700

    done

--- src/pages/game-list/index.tsx
+++ src/pages/game-list/index.tsx
@@ -1,4 +1,4 @@
-import { getGameList } from "@/store/slices/gameSlice";
+import { getGameList, getOptions } from "@/store/slices/gameSlice";
 import { AppDispatch } from "@/store/store";
 import { Box, Typography } from "@mui/material"
 import Grid from '@mui/material/Grid2';
@@ -7,23 +7,176 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 import { getValueByLang } from '@/utils/getValueByLang';
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
 const GameList = () => {
   const { t, i18n } = useTranslation();
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
   const [games, setGames] = useState([]);
+  const [options, setOptions] = useState<any>({});
+  const [filters, setFilters] = useState({
+    gameCategories: 'slot',
+    gameCurrencies: 'thb',
+    gameLangs: 'en',
+    gameStatus: '0',
+    gameTags: 'EVENT',
+    jackpots: 'JACKPOT',
+    volatilities: 'HIGH',
+    rtps: '99'
+  })
+
 
   const fetchGameList = async () => {
-    const { payload } = await dispatch(getGameList({}))
+    const { payload } = await dispatch(getGameList({ gameStatus: 2 }))
     setGames(payload.data.data)
+  }
+
+  const fetchOptions = async () => {
+    const { payload } = await dispatch(getOptions())
+    setOptions(payload)
   }
 
   useEffect(() => {
     fetchGameList()
+    fetchOptions()
   }, [])
 
   return (
     <Grid container columns={12}>
+      <Grid size={12}>
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label">Categories</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={filters.gameCategories}
+            label="Categories"
+            onChange={(event) => setFilters({ ...filters, gameCategories: event.target.value })}          >
+            {
+              options?.gameCategories?.map((category: string) => {
+                return <MenuItem key={category} value={category}>{category}</MenuItem>
+              })
+            }
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label2">Currencies</InputLabel>
+          <Select
+            labelId="demo-simple-select-label2"
+            id="demo-simple-select2"
+            value={filters.gameCurrencies}
+            label="Categories"
+            onChange={(event) => setFilters({ ...filters, gameCurrencies: event.target.value })}          >
+            {
+              Object.keys(options.gameCurrencies ?? {}).map((key) => (
+                <MenuItem key={key} value={options.gameCurrencies[key]}>{key}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label3">Lang</InputLabel>
+          <Select
+            labelId="demo-simple-select-label3"
+            id="demo-simple-select3"
+            value={filters.gameLangs}
+            label="Lang"
+            onChange={(event) => setFilters({ ...filters, gameLangs: event.target.value })}          >
+            {
+              Object.keys(options.gameLangs ?? {}).map((key) => (
+                <MenuItem key={key} value={options.gameLangs[key]}>{key}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label4">Status</InputLabel>
+          <Select
+            labelId="demo-simple-select-label4"
+            id="demo-simple-select4"
+            value={filters.gameStatus}
+            label="Lang"
+            onChange={(event) => setFilters({ ...filters, gameStatus: event.target.value })}          >
+            {
+              Object.keys(options.gameStatus ?? {}).map((key) => (
+                <MenuItem key={key} value={options.gameStatus[key]}>{key}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label5">Tags</InputLabel>
+          <Select
+            labelId="demo-simple-select-label5"
+            id="demo-simple-select5"
+            value={filters.gameTags}
+            label="Lang"
+            onChange={(event) => setFilters({ ...filters, gameTags: event.target.value })}          >
+            {
+              Object.keys(options.gameTags ?? {}).map((key) => (
+                <MenuItem key={key} value={options.gameTags[key]}>{key}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label6">jackpots</InputLabel>
+          <Select
+            labelId="demo-simple-select-label6"
+            id="demo-simple-select6"
+            value={filters.jackpots}
+            label="Lang"
+            onChange={(event) => setFilters({ ...filters, jackpots: event.target.value })}          >
+            {
+              Object.keys(options.jackpots ?? {}).map((key) => (
+                <MenuItem key={key} value={options.jackpots[key]}>{key}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label7">volatilities</InputLabel>
+          <Select
+            labelId="demo-simple-select-label7"
+            id="demo-simple-select7"
+            value={filters.volatilities}
+            label="Lang"
+            onChange={(event) => setFilters({ ...filters, volatilities: event.target.value })}          >
+            {
+              Object.keys(options.volatilities ?? {}).map((key) => (
+                <MenuItem key={key} value={options.volatilities[key]}>{key}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ marginRight: 2, marginBottom: 2, minWidth: '150px' }}>
+          <InputLabel id="demo-simple-select-label10">rtps</InputLabel>
+          <Select
+            labelId="demo-simple-select-label10"
+            id="demo-simple-select10"
+            value={filters.rtps}
+            label="Categories"
+            onChange={(event) => setFilters({ ...filters, rtps: event.target.value })}          >
+            {
+              options?.rtps?.map((category: string) => {
+                return <MenuItem key={category} value={category}>{category}</MenuItem>
+              })
+            }
+          </Select>
+        </FormControl>
+
+      </Grid>
       <Box>
         <Typography variant="h3">{t('gameList')}</Typography>
       </Box>
@@ -45,9 +198,7 @@ const GameList = () => {
                   getValueByLang(game.gameName, i18n.language)
                 }
               </h3>
-              <p className="text-gray-600 mt-2">
-                <div dangerouslySetInnerHTML={{ __html: getValueByLang(game.description, i18n.language) }} />
-              </p>
+              <div className="text-gray-600 mt-2" dangerouslySetInnerHTML={{ __html: getValueByLang(game.description, i18n.language) }} />
 
               <button onClick={() => navigate(
                 `/game/${game.gameId}`
